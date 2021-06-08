5ef3361bcec3f66518010f359fcdc52c4facd560
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2021-06-08 17:50:02 +0800

    add abstract func and main func

---
+++ businessfunc.py
@@ -0,0 +1,29 @@
+from customizetool import *
+from getvatinvoicedtl import *
+from getgenericocr import *
+from getbaidutoken import *
+
+def VAT2TXT(parameter:str):
+    tokenstr = getBaiduToken(conf_path=parameter)
+    inpath = getSourceFilePath(parafile=parameter)
+    outputpath = getOutputPath(parafile=parameter)
+    fileset = overtraversalFile(dirpath=inpath)
+    rtnDataset = []
+    for file in fileset:
+        resultpath = ''.join([inpath,file])
+        print(resultpath)
+        data = getVATInvoice(
+        token=tokenstr,
+        imgpath=resultpath,
+        dtlflag=False
+        )
+        if data[0] == 0:
+            rtnDataset.append('/'.join(data[1]))
+        else:
+            raise Exception('error message = ',data)
+    print(rtnDataset)
+    writeData2File(fpath=outputpath,fname='invoice_list.txt',indata=changeResult2Data(indata=rtnDataset))
+    return rtnDataset
+
+def OCR2TXT():
+    pass
---
+++ mainfunc.py
@@ -0,0 +1,5 @@
+from businessfunc import VAT2TXT
+
+if __name__ == '__main__':
+    para = './conf/para.conf'
+    VAT2TXT(parameter=para)
